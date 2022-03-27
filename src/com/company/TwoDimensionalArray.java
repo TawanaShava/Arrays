5ac58c6d74d8cc0d0cb6213e6365787acb13e593
TawanaShava <94750339+TawanaShava@users.noreply.github.com>
2022-03-27 23:39:29 +0200

    Update TwoDimensionalArray.java

--- src/com/company/TwoDimensionalArray.java
+++ src/com/company/TwoDimensionalArray.java
@@ -3,38 +3,28 @@ package com.company;
 import java.util.Arrays;
 
 public class TwoDimensionalArray {
-    public static void main(String[] args){
-
-      int diagonal=0;
-        int[][] arr = {
-                { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
-                { 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
-                { 0 , 0 , 2 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
-                { 0 , 0 , 0 , 3 , 0 , 0 , 0 , 0 , 0 , 0 },
-                { 0 , 0 , 0 , 0 , 4 , 0 , 0 , 0 , 0 , 0 },
-                { 0 , 0 , 0 , 0 , 0 , 5 , 0 , 0 , 0 , 0 },
-                { 0 , 0 , 0 , 0 , 0 , 0 , 6 , 0 , 0 , 0 },
-                { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 7 , 0 , 0 },
-                { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 8 , 0 },
-                { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 9 }
-        };
-        int [][] matrix=new int[10][10];
-
-        for (int i = 0; i< arr.length;i++){
-            for (int j = 0;j<arr.length;j++){
-                matrix[i][j]=arr[i][j];
-                if(i==j)diagonal +=arr[i][j];
-                System.out.println(arr[i][j]);
+    public static void main(String[] args) {
+        
+        int n = 10 , i , j , sum = 0;
+        int matrix [][]= new int [n][n];
+        
+        //entering numbers from 0 to 9 on the diagonal and 0 outside the diagonal into the table
+        
+        for (i = 0 ; i<matrix.length;i++){
+            for(j = 0; j< matrix.length; j++){
+                if (i == j){
+                    matrix[i][j] = i;
+                    sum += matrix[i][j];
+                } else {
+                    matrix[i][j] = 0;
+                }
+                System.out.println(matrix[i][j] + " ");
             }
-
-
-
-
-
+            System.out.println();
         }
-
         System.out.println(" ");
-        System.out.println("The sum of the diagonal is:" + diagonal);
+        System.out.println("The sum of the diagonal in the array is equal to: " + sum);
+
 
     }
 }
